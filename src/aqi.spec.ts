39961480864a9f705f39c4170af5e85c9889cf67
Phips Peter <pspeter333@gmail.com>
2020-10-05 21:16:49 -0700

    Fix formatting

--- src/aqi.spec.ts
+++ src/aqi.spec.ts
@@ -6,10 +6,10 @@ describe("aqi", () => {
             // Based on San Francisco sensors
             expect(Math.round(calculatAQI(14.28, 72, 60))).toEqual(30);
         });
-        
+
         it("should throw for high numbers", () => {
             // Based on Battle Mountain sensors
             expect(() => calculatAQI(1000, 25, 77)).toThrow();
-        })
+        });
     });
 });
