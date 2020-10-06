9681bd0bc995f252f464b954084e70c2bb7fc6ac
Phips Peter <pspeter3@users.noreply.github.com>
2020-10-05 21:18:23 -0700

    Add AQI calculation (#7)
    * Add AQI calculation
    
    Calculate the AQI based on particles, humidity, and temperature.
    
    * Fix formatting

---
+++ src/aqi.spec.ts
@@ -0,0 +1,15 @@
+import { calculatAQI } from "./aqi";
+
+describe("aqi", () => {
+    describe("calculateAQI", () => {
+        it("should return the EPA value", () => {
+            // Based on San Francisco sensors
+            expect(Math.round(calculatAQI(14.28, 72, 60))).toEqual(30);
+        });
+
+        it("should throw for high numbers", () => {
+            // Based on Battle Mountain sensors
+            expect(() => calculatAQI(1000, 25, 77)).toThrow();
+        });
+    });
+});
